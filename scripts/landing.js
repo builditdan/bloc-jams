ca8f458b13facaf0d88f540ec779bb725c0f4bba
builditdan <daniels111@gmail.com>
2016-03-27 13:22:48 -0400

    Refactored javascript transitiion code for points

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,32 +1,18 @@
-      var animatePoints = function() {
+      var revealPoints = function() {
         
         var points = document.getElementsByClassName('point');
-              
-        var revealFirstPoint = function() {
-          points[0].style.opacity = 1;
-          points[0].style.transform = "scaleX(1) translateY(0)";
-          points[0].style.msTransform = "scaleX(1) translateY(0)";
-          points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-        };
+
+        var revealPoint = function(point_no) {
+          points[point_no].style.opacity = 1;
+          points[point_no].style.transform = "scaleX(1) translateY(0)";
+          points[point_no].style.msTransform = "scaleX(1) translateY(0)";
+          points[point_no].style.WebkitTransform = "scaleX(1) translateY(0)";
+        }
         
-        var revealSecondPoint = function() {
-          points[1].style.opacity = 1;
-          points[1].style.transform = "scaleX(1) translateY(0)";
-          points[1].style.msTransform = "scaleX(1) translateY(0)";
-          points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-        };
-        
-        var revealThirdPoint = function() {
-          points[2].style.opacity = 1;
-          points[2].style.transform = "scaleX(1) translateY(0)";
-          points[2].style.msTransform = "scaleX(1) translateY(0)";
-          points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-        };
-        
-        revealFirstPoint();
-        revealSecondPoint();
-        revealThirdPoint();
-        
+        for (i = 0; i < points.length; i++) {
+          revealPoint(i);
+        }
+       
       };
         
   
