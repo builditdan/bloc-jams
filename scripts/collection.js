42f0abc6cfb5d306ccdf9ad8cdc9ba3a0374d605
builditdan <daniels111@gmail.com>
2016-06-07 08:00:43 -0400

    adjusting base url

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,12 +1,12 @@
 var buildCollectionItemTemplate = function() {
   var template = 
     '<div class="collection-album-container column fourth">'
-  + ' <img src="{{site.baseurl }}/assets/images/album_covers/01.png"/>'
+  + ' <img src="assets/images/album_covers/01.png"/>'
   + ' <div class="collection-album-info caption">'
   + '   <p>'
-  + '      <a class="album-name" href="{{site.baseurl }}/album.html"> The Colors </a>'
+  + '      <a class="album-name" href="album.html"> The Colors </a>'
   + '      <br/>'
-  + '      <a href="{{site.baseurl }}/album.html"> Pablo Picasso </a>'
+  + '      <a href="album.html"> Pablo Picasso </a>'
   + '      <br/>'
   + '      X songs'
   + '      <br/>'
